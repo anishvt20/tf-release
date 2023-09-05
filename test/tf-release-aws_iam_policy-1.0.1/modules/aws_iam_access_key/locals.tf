8cb4f4d0f674324eccc6e16ce6881b516ee1e3e7
Aneesh <anishvt20@gmail.com>
2023-09-05 11:21:34 +0200

    feat: cleanup

--- test/tf-release-aws_iam_policy-1.0.1/modules/aws_iam_access_key/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  user    = try(coalesce(var.config.user), null)
-  pgp_key = try(coalesce(var.config.pgp_key), null)
-  status  = try(coalesce(var.config.status), null)
-}
-
--- test/tf-release-aws_iam_policy-1.0.1/modules/aws_iam_access_key/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_access_key" "this" {
-  user    = local.user
-  pgp_key = local.pgp_key
-  status  = local.status
-}
-
